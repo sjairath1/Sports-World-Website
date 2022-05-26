905e98d791493cd35c741422a022ac6695eaa9ed
sjairath1 <sakshamjairath2026@u.northwestern.edu>
2022-05-25 19:14:28 -0500

    hw4

--- homework/hw04/your_task/js/scripts.js
+++ homework/hw04/your_task/js/scripts.js
@@ -17,18 +17,93 @@ const search = (ev) => {
 }
 
 const getTracks = (term) => {
+    document.querySelector("#tracks").innerHTML = "";
+
     console.log(`
         get tracks from spotify based on the search term
         "${term}" and load them into the #tracks section 
         of the DOM...`);
+
+        fetch(baseURL + "?type=track&limit=5&q=" + term)
+        .then(response => response.json())
+        .then(tracks => {
+            console.log(tracks)
+
+        if(tracks.length == 0){ 
+                document.querySelector("#tracks").innerHTML = "No tracks found.";
+        }
+        for (const track of tracks) {
+            document.querySelector("#tracks").innerHTML += `
+        `;
+        document.querySelector("#tracks").innerHTML += `
+        <button class="track-item preview" data-preview-track="${track.preview_url}" onclick="handleTrackClick(event);">
+            <img src="${track.album.image_url}" alt="track image">
+            <i class="fas play-track fa-play" aria-hidden="true"></i>
+            <div class="label">
+                <h2>${track.name}</h2>
+            <p>
+                ${track.artist.name}
+            </p>
+        </div> 
+    </button>
+  `;
+   
+            
+            //console.log()
+        }
+    })
 };
+    
+
+
+        
+
+
+        
 
 const getAlbums = (term) => {
     console.log(`
         get albums from spotify based on the search term
         "${term}" and load them into the #albums section 
         of the DOM...`);
+        document.querySelector("#albums").innerHTML = "";
+
+        fetch(baseURL + "?type=album&q=" + term)
+        .then(response => response.json())
+        .then(albums => {
+            console.log(albums)
+
+        if(albums.length == 0){ 
+                document.querySelector("#albums").innerHTML = "No albums found.";
+        }
+        for (const album of albums) {
+            document.querySelector("#albums").innerHTML += `
+         
+        `;
+        document.querySelector("#albums").innerHTML += `
+        <section class="album-card" id="${album.id}">
+    <div>
+        <img src="${album.image_url}" alt="album image">
+        <h2>${album.name}</h2>
+        <div class="footer">
+            <a href="${album.spotify_url}" target="_blank">
+                view on spotify
+            </a>
+        </div>
+    </div>
+</section>
+  `;
+            //console.log()
+        }
+    })
+    
 };
+
+
+/* line 80   /* <p>${album.name}</p> /* 
+    line 37             <p>${track.name}</p>
+
+*/
 
 const getArtistHTML = (artist) => {
     if (!artist.image_url){
@@ -37,10 +112,10 @@ const getArtistHTML = (artist) => {
 
     return `<section class="artist-card" id="3Nrfpe0tUJi4K4DXYWgMUX">
     <div>
-        <img src="https://i.scdn.co/image/0c9057cb30520f9f883a220051260fc66a2f3ffa">
-        <h2>BTS</h2>
+        <img src="${artist.image_url}" alt="artist image">
+        <h2>${artist.name}</h2>
         <div class="footer">
-            <a href="https://open.spotify.com/artist/3Nrfpe0tUJi4K4DXYWgMUX" target="_blank">
+            <a href="${artist.spotify_url}" target="_blank">
                 view on spotify
             </a>
         </div>
@@ -53,6 +128,7 @@ const getArtist = (term) => {
         get artists from spotify based on the search term
         "${term}" and load the first artist into the #artist section 
         of the DOM...`);
+
         document.querySelector("#artist").innerHTML = "";
         fetch(baseURL + "?type=artist&q=" + term)
         .then(response => response.json())
@@ -70,9 +146,16 @@ const getArtist = (term) => {
         })
 };
 
+
 const handleTrackClick = (ev) => {
     const previewUrl = ev.currentTarget.getAttribute('data-preview-track');
     console.log(previewUrl);
+
+    document.querySelector("#current-track").innerHTML=ev.currentTarget.innerHTML; 
+
+
+    audioPlayer.setAudioFile(previewUrl);
+    audioPlayer.play();
 }
 
 document.querySelector('#search').onkeyup = (ev) => {
@@ -82,4 +165,5 @@ document.querySelector('#search').onkeyup = (ev) => {
         ev.preventDefault();
         search();
     }
-};
+}
+        
